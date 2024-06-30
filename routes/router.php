e3a6b385da7da2aaa792f110e26ab293550d6e3f
Konstantinos Spyropoulos <58692615+KostasSpyropoylos@users.noreply.github.com>
2024-06-30 19:52:54 +0300

    added new route

--- routes/router.php
+++ routes/router.php
@@ -6,12 +6,13 @@ $requestUri = strtok($_SERVER['REQUEST_URI'], '?');
 // Define your routes
 $routes = [
     '/' => 'components/pages/listings-component/listings-component.php',
-    '/listing-details' => 'components\pages\house-details-component\house-details-component.php',
-    '/create-listing' => 'components\pages\create-listing-component\create-listing.component.php',
+    '/listing-details' => 'components/pages/house-details-component/house-details-component.php',
+    '/create-listing' => 'components/pages/create-listing-component/create-listing.component.php',
     '/login' => 'components/pages/login-component/login-component.php',
     '/process' => 'components/pages/login-component/process.php',
-    '/logout' => 'components\layout\logout.php',
-    '/register' => 'components\pages\signup-component\signup-component.php',
+    '/logout' => 'components/layout/logout.php',
+    '/register' => 'components/pages/signup-component/signup-component.php',
+    '/handle-create-listing' => 'components/pages/create-listing-component/create-listing.php',
 ];
 
 // Check if the request URI matches a defined route
@@ -20,6 +21,6 @@ if (array_key_exists($requestUri, $routes)) {
 } else {
     // Handle 404 Not Found
     http_response_code(404);
-    require ('components\pages\404.php');
+    require ('components/pages/404.php');
     die();
 }
